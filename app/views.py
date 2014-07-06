15daa1c24e5464d8bf60c41b357b0040050e979e
Nadja Rhodes <narhodes1@gmail.com>
2014-07-05 17:37:37 -0700

    Templates: separating layout from logic

--- app/views.py
+++ app/views.py
@@ -1,6 +1,22 @@
+from flask import render_template #Jinja2 templating engine
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	user = { 'nickname': 'iconix' } # fake user
+	#user = {}
+	posts = [ # fake array of posts
+		{ 
+			'author': { 'nickname': 'John' }, 
+			'body': 'Beautiful day in Seattle!' 
+		},
+		{ 
+			'author': { 'nickname': 'Susan' }, 
+			'body': 'The Avengers movie was so cool!' 
+		}
+	]
+	return render_template("index.html",
+		title = 'Home',
+		user = user,
+		posts = posts)
